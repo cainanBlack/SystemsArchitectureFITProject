c7e1d82fe8e70b755f381a14fea406914c4fa2ab
Joseph allen <jallen1175@gmail.com>
2024-12-01 21:28:29 -0500

    functionality for function 21_85 added, as well as the tests proving it works

--- SystemsArchitectureFITProject/FunctionTests/21_18_Function_Test.py
+++ SystemsArchitectureFITProject/FunctionTests/21_18_Function_Test.py
@@ -9,16 +9,16 @@ x = 20  # Average value of x (x with bar)
 lambda_ = 2  # Wavelength (known)
 d_i = None  # d_i is missing
 
-# Solve for the missing d_i using function2_18
-missingDi = functions.function2_18(f=f, x=x, lambda_=lambda_)
+# Solve for the missing d_i using function21_18
+missingDi = functions.function21_18(f=f, x=x, lambda_=lambda_)
 print("Solved for missing d_i:", missingDi)
 
 # Case 2: Given values and solve for the missing f
 d_i = 5  # Distance (known)
 f = None  # Frequency (f with bar) is missing
 
-# Solve for the missing f using function2_18
-missingF = functions.function2_18(x=x, lambda_=lambda_, d_i=d_i)
+# Solve for the missing f using function21_18
+missingF = functions.function21_18(x=x, lambda_=lambda_, d_i=d_i)
 print("Solved for missing f:", missingF)
 
 # Case 3: Given values and solve for the missing x
@@ -26,8 +26,8 @@ d_i = 5  # Distance (known)
 lambda_ = 2  # Wavelength (known)
 f = 10  # Average frequency (f with bar)
 
-# Solve for the missing x using function2_18
-missingX = functions.function2_18(f=f, lambda_=lambda_, d_i=d_i)
+# Solve for the missing x using function21_18
+missingX = functions.function21_18(f=f, lambda_=lambda_, d_i=d_i)
 print("Solved for missing x:", missingX)
 
 # Case 4: Given values and solve for the missing lambda
@@ -35,6 +35,6 @@ f = 10  # Frequency (f with bar)
 x = 20  # Value of x (x with bar)
 d_i = 5  # Distance (known)
 
-# Solve for the missing lambda using function2_18
-missingLambda = functions.function2_18(f=f, x=x, d_i=d_i)
+# Solve for the missing lambda using function21_18
+missingLambda = functions.function21_18(f=f, x=x, d_i=d_i)
 print("Solved for missing lambda:", missingLambda)

e3a07cdba31b58609f04de0e36fd9d4c5d384caa
SandmanDota <cam100597@hotmail.com>
2024-12-06 16:36:20 -0500

    master-21_19 add doxygen

--- SystemsArchitectureFITProject/FunctionTests/21_19_Function_Test.py
+++ SystemsArchitectureFITProject/FunctionTests/21_19_Function_Test.py
@@ -3,16 +3,19 @@ from SystemsArchitectureFITProject.Functions.Functions import Functions
 # Create an instance of the Functions class
 functions = Functions()
 
+# Case 1 Solve for GPF
 wp = 1
 theta_x = 45
 
 print("Solved for gpf:", functions.function21_19(wp=wp, theta_x=theta_x))
 
+# Case 2 Solve for WP
 gpf = 1
 theta_x = 45
 
 print("Solved for wp:", functions.function21_19(gpf=gpf, theta_x=theta_x))
 
+# Case 3 Solve for Theta_X
 gpf = 2
 wp = 3
 
